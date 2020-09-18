d809f01951c378f9a75e4ebc24bf1f04afdc8983
@blake_r38 <39965542+crash0ver1d3@users.noreply.github.com>
2020-09-18 14:52:50 -0500

    Powershell framework to find and remove files by name on Windows Active Directory domain.
    The file variables can be modified to suit your needs, same for the paths of where to find the files, along with the operating system where statements.
    Please test this script on a test environment so that you know what to expect. This does work successfully and only removes the files you tell it to.
    This script was purpose built to cleanup old utilities on servers built with a template.
    
    Plan to add hash search capability and reporting in future update.

---
+++ Domain-Seek-And-Remove-Files.ps1
@@ -0,0 +1,62 @@
+﻿##############################################################################
+#  Script: Get-LocalMembership-Domain.ps1
+#    Date: 2020.09.18
+# Version: 3.5
+#  Author: Blake Regan @blake_r38
+# Purpose: Identify accounts in local groups on domain joined windows workstations
+#   Legal: Script provided "AS IS" without warranties or guarantees of any
+#          kind.  USE AT YOUR OWN RISK.  Public domain, no rights reserved.
+
+
+
+$remove_psexec= {
+
+cd "C:\Tools"
+
+$target = dir -file | Get-ChildItem 
+$psexec="PsExec.exe"
+$tcpview="Tcpview.exe"
+$adexplorer="AdExplorer.exe"
+$targets = dir -file | Get-ChildItem
+foreach ($target in $targets)
+{
+   if (Test-Path -Path "C:\Tools") 
+   { 
+    if ($target.Name -like $psexec -or $target.Name -like $tcpview)
+    {
+    remove-item $target.Name -force
+    write-host
+    write-host "$($target.Name) removed"
+    }
+    }
+}
+$targets=$null
+
+cd "C:\Tools\Extra's"
+
+$targets = dir -file | Get-ChildItem
+foreach ($target in $targets)
+{
+    if (Test-Path -Path "C:\Tools\Extra's")
+    {
+    if ($target.Name -like $psexec -or $target.Name -like $adexplorer -or $target.Name -like $tcpview)
+    {
+    remove-item $target.Name -force
+    write-host
+    write-host "$($target.Name) removed"
+    }
+    }
+}
+exit
+}
+
+$root = [ADSI]"LDAP://RootDSE"
+$DOMAIN = $root.defaultNamingContext
+
+
+$hosts=Get-ADComputer -filter * -SearchBase "OU=Servers,$DOMAIN" -Properties Name,OperatingSystem  | where-object {$_.OperatingSystem -like "Windows Server 2008*" -or $_.OperatingSystem -like "Windows Server 2012*" -or $_.OperatingSystem -like "Windows Server 2016*" -or $_.OperatingSystem -like "Windows Server 2019*"} | select-object -Property Name, OperatingSystem
+foreach ($server in $hosts)
+{
+    Invoke-Command -ComputerName $server.Name -ScriptBlock $remove_psexec
+
+}
